e8d3155f1c0204d2f741590a691660024617e7c3
gvladislav-52 <gvladislav_52@mail.ru>
2026-02-14 09:16:10 +0300

    Initial Commit

---
+++ JsonTest/JsonTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonTestApp.swift
+//  JsonTest
+//
+//  Created by gvladislav-52 on 14.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct JsonTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
